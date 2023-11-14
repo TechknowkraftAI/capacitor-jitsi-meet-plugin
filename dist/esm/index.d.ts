182a409c01df1760ff62f79ef44111b53a96ac1a
Sudhirkumarrao Allada <sud5868@gmail.com>
2023-11-15 04:50:49 +0530

    changes

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CustomJitsiMeetPlugin } from './definitions';
-declare const CustomJitsiMeet: CustomJitsiMeetPlugin;
-export * from './definitions';
-export { CustomJitsiMeet };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CustomJitsiMeet = registerPlugin('CustomJitsiMeet', {
-    web: () => import('./web').then(m => new m.CustomJitsiMeetWeb()),
-});
-export * from './definitions';
-export { CustomJitsiMeet };
-//# sourceMappingURL=index.js.map
